0f26669e9d34858774185d1ec0a4a5f45653314c
farha-neu <farhajw@gmail.com>
2018-07-19 14:19:24 -0700

    color changed

--- src/components/Fruit/Fruit.js
+++ src/components/Fruit/Fruit.js
@@ -4,7 +4,7 @@ import "./Fruit.css";
 
 
 const Fruit = props => (
-    <div className={props.incorrect===1?"animate-style col-lg-3 col-md-4 col-sm-6 mb-4":"col-lg-3 col-md-4 col-sm-6 col-12 mb-4"}>
+    <div className={props.incorrect===1?"animate-style col-lg-3 col-md-4 col-sm-6 mb-4":"col-lg-3 col-md-4 col-sm-6 mb-4"}>
         <img className="img-fluid img-thumbnail rounded-circle" src={props.src} alt={props.alt} key={props.id} 
         onClick={() => props.clickFruit(props.id)}/>
     </div>
