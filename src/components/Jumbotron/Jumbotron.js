9df4dda5fce5b14ca8421f803f0a797ab062ae33
farha-neu <farhajw@gmail.com>
2018-07-19 21:14:20 -0700

    bg

--- src/components/Jumbotron/Jumbotron.js
+++ src/components/Jumbotron/Jumbotron.js
@@ -8,7 +8,7 @@ const Jumbotron = props => {
     return(
         <div className="jumbotron jumbotron-fluid text-center">
                 <div className="container">
-                    <h1 className="display-4"><i className="fab fa-apple"></i> Kids Fruit Memory</h1>
+                    <h1 className="display-4"><i className="fab fa-apple"></i> Kids Fruit Memory Game!</h1>
                     <p className="lead">Click a fruit to earn points, but don't click on any more than once!</p>
                 </div>
         </div>
